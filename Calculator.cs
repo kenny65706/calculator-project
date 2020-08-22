2726f228150cd6d208cf22ca7e37ba491f5ede57
Kenny65706 <kenny65706@hotmail.com>
2020-08-22 01:57:54 -0500

    trimmed down the function, added switch

--- Calculator.cs
+++ Calculator.cs
@@ -5,17 +5,11 @@ namespace calculator
 	
 	class Calculator
 	{
-		static string Addition(string output)
+		
+		
+		static int Addition(int opta, int optb)
 		{
-			Console.WriteLine(output);
-			Console.WriteLine("Please Type a number");
-			int input_one = int.Parse(Console.ReadLine());
-			Console.WriteLine("+");
-			int input_two = int.Parse(Console.ReadLine());
-			int sum = input_one + input_two;
-			Console.WriteLine(string.Format("The sum of the two numbers is {0}...", sum)); 
-			Console.ReadKey();
-			return output;
+			return opta + optb;
 		}
 		static string Subtraction(string output)
 		{
@@ -29,16 +23,9 @@ namespace calculator
 			return output;
 		}
 		
-		static string Multiplication(string output)
+		static int Multiplication(int opta,int optb)
 		{
-			Console.WriteLine(output);
-			Console.WriteLine("Please Type a number");
-			int input_one = int.Parse(Console.ReadLine());
-			Console.WriteLine("*");
-			int input_two = int.Parse(Console.ReadLine());
-			int product = input_one * input_two;
-			Console.WriteLine(string.Format("The product of the two numbers is {0}...", product)); 
-			return output;
+			return opta * optb;
 		}
 		static string Division(string output)
 		{
@@ -55,14 +42,51 @@ namespace calculator
 		//bonus for making the result inclue remainder for division
 		//need to debug input errors for inputing non integers
 		//change wording
+		//implement a loop that keeps taking input and maintains current answer. uses answer for oncoming operations
+		//need a clear input to clear answer
+		//need an exit
+		//use read line to take input instead of command line (string.split hint hint)
+		//looking for c# cast integer to decimal 
 		static void Main(string[] args)
 		{
+			int inp1 = int.Parse(args[0]);
+			int inp2 = int.Parse(args[2]);
+			int answer = 0;
+			//this is a test
+			int[] toys = new int[5];
+			toys[0] = 4;
+			toys[1] = 2;
+			toys[2] = 5;
+			toys[3] = 6;
+			toys[4] = 8;
 			
-			Addition("What is the first number number you would like to add?");
+			/*Addition("What is the first number number you would like to add?");
 			Subtraction("What is the number you want to subtract from?");
 			Multiplication("What is the first number to want to multiply.");
 			Division("What is the number you want to divide?")
-			Console.ReadKey();
+			Console.ReadKey();*/
+			
+			//Console.WriteLine(args.Length);
+			for (int i = 0; i < args.Length; i++)
+			{
+				Console.WriteLine(args[i]);
+
+			}
+			
+			switch (args[1])
+			{
+				case "x": 
+					answer = Multiplication(inp1, inp2);
+					break;
+				case "+":
+					answer = Addition(inp1, inp2);
+					break;
+				
+				default:
+					Console.WriteLine(string.Format("We don't know what operation {0} is.", args[1]));
+					return;
+			}
+			Console.WriteLine(answer);
 		}
 	}
 	
